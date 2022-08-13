b0f9347cc0a0f17697b48378131fd1410aa44f0f
Steerpike <imthekoul@gmail.com>
2022-08-13 23:28:45 +0300

    App configuration

---
+++ app.js
@@ -0,0 +1,59 @@
+const express = require('express');
+const path = require('path');
+const indexRouter = require('./routes/index');
+const messageRouter = require('./routes/messages');
+const userRouter = require('./routes/users');
+const session = require('express-session');
+const MongoStore = require('connect-mongo');
+const mongooseClient = require('./config/database');
+const passport = require('passport');
+
+// ---- GENERAL SETUP
+
+const app = express();
+require('dotenv').config();
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+// ---- SESSION SETUP
+
+app.use(
+  session({
+    secret: process.env.MONGO_SESSION_KEY,
+    resave: false,
+    saveUninitialized: true,
+    store: MongoStore.create({
+      client: mongooseClient,
+      collectionName: 'sessions',
+    }),
+    cookie: {
+      maxAge: 1000 * 60 * 60 * 24,
+    },
+  })
+);
+
+//---- PASSPORT AUTHENTICATION
+require('./config/passport');
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+//Logs
+app.use((req, res, next) => {
+  console.log(req.session);
+  console.log(req.user);
+  next();
+});
+
+//---- ROUTES
+
+app.use('/', indexRouter);
+app.use('/messages', messageRouter);
+app.use('/users', userRouter);
+
+app.listen(process.env.PORT, () => console.log('Listening on port '));
