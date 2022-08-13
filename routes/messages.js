92a171f9ab656b8e2edb42a49e0cd24129cea01e
Steerpike <imthekoul@gmail.com>
2022-08-13 23:26:54 +0300

    Application routes

---
+++ routes/messages.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const router = express.Router();
+const messageController = require('../controllers/messageController');
+
+router.get('/', messageController.index);
+
+router.get('/create', messageController.message_create_get);
+
+router.post('/create', messageController.message_create_post);
+
+module.exports = router;
---
+++ routes/users.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const router = express.Router();
+const userController = require('../controllers/userController');
+
+router.get('/signup', userController.user_create_get);
+
+router.post('/signup', userController.user_create_post);
+
+router.get('/login', userController.user_login_get);
+
+router.post('/login', userController.user_login_post);
+
+router.get('/logout', userController.user_logout_get);
+
+module.exports = router;
